37eea4d7115f968bfc784b5c8ab40800aff3b566
Ademir <ademir.m.junior@gmail.com>
2021-08-01 17:09:43 -0300

    Develop action of the 'CLEAR' button

--- app/src/main/java/br/edu/utfpr/listadecomprasemmercados/MainActivity.java
+++ app/src/main/java/br/edu/utfpr/listadecomprasemmercados/MainActivity.java
@@ -3,12 +3,29 @@ package br.edu.utfpr.listadecomprasemmercados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextNomeDoItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextNomeDoItem = findViewById(R.id.editTextNomeDoItem);
+    }
+
+    public void limparCampos(View view) {
+        editTextNomeDoItem.setText(null);
+
+        editTextNomeDoItem.requestFocus();
+
+        Toast.makeText(this,
+                R.string.todos_os_campos_foram_limpos,
+                Toast.LENGTH_LONG).show();
     }
 }
